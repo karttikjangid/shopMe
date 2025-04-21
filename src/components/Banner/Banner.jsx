ad665622650b9d4c3beff1f0d4020095a450b683
Kartik Jangid <kartik.24bcs10259@sst.scaler.com>
2025-04-22 01:10:55 +0530

    completed some part

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -27,8 +27,7 @@ const Banner = () => {
               data-aos="fade-up"
               className="text-sm text-gray-500 tracking-wide leading-5"
             >
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
-              reiciendis inventore iste ratione ex alias quis magni at optio
+              Here at Shop me u can trust us for your fashion choice !!
             </p>
             <div className="flex flex-col gap-4">
               <div data-aos="fade-up" className="flex items-center gap-4">
